0f7c18a4a883c053b35ddfaba36e25ac829faeca
Aleksandr <naberezhnyy.an@gmail.com>
2023-08-16 12:39:08 +0200

    instal google packages

--- s3.js
+++
@@ -1,3 +0,0 @@
-import { AWS_ACCES_KEY_ID,AWS_SECRET_ACCESS_KEY } from '$env/static/private';
-
-console.log(AWS_ACCES_KEY_ID, AWS_SECRET_ACCESS_KEY);
---
+++ src/routes/gcs/+server.ts
@@ -0,0 +1,1 @@
+import { json } from '@sveltejs/kit';
