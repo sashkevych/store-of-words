5d484f0af148ef8dcdbe972c065ba93a57ea3165
Aleksandr <naberezhnyy.an@gmail.com>
2023-08-16 13:13:24 +0200

    init fs

--- src/lib/api/google-api.js
+++ src/lib/api/google-api.js
@@ -1,7 +1,6 @@
 import { bigquery, storage } from './client.js';
 
 
-
 export async function qwe() {
 	const [job] = await bigquery.createQueryJob('SELECT * FROM `nativ_table.store-table`');
 	const [rows] = await job.getQueryResults();
