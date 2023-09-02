12ba7f0cb1fe3f0bf7bfa38c57c20f7498008349
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-02 13:15:17 +0200

    start work with weekly

--- src/lib/components/weekly-repeats/Weekly.svelte
+++ src/lib/components/weekly-repeats/Weekly.svelte
@@ -1,1 +1,19 @@
-<div id="weekly">Weekly</div>
+<div id="weekly">
+    <div class="test">
+        <div class="card-box">
+            Card box
+        </div>
+    </div>
+</div>
+
+
+<style>
+    .test {
+        border: 1px solid black;
+        height: 100%;
+    }
+    .card-box {
+        border: 1px solid brown;
+        border-radius: 4px;
+    }
+</style>
