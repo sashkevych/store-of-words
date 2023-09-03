0ae71bfa88e6148c2bd00068c5442a60e57f6ac1
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-03 16:16:39 +0200

    Add media for sidebar

--- src/lib/components/side-bar/MediaQuery.svelte
+++ src/lib/components/side-bar/MediaQuery.svelte
@@ -1,39 +1,40 @@
 <script>
-    import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-    export let query;
+	export let query;
 
-    let mql;
-    let mqlListener;
-    let wasMounted = false;
-    let matches = false;
+	let mql;
+	let mqlListener = (v) => (matches = v.matches);
+	let wasMounted = false;
+	let matches = false;
 
-    onMount(() => {
-        wasMounted = true;
-        return () => {
-            removeActiveListener();
-        };
-    });
+	onMount(() => {
+		wasMounted = true;
+		return () => {
+			removeActiveListener();
+		};
+	});
 
-    $: {
-        if (wasMounted) {
-            removeActiveListener();
-            addNewListener(query);
-        }
-    }
+	$: {
+		if (wasMounted) {
+			removeActiveListener();
+			addNewListener(query);
+		}
+	}
 
-    function addNewListener(query) {
-        mql = window.matchMedia(query);
-        mqlListener = v => matches = v.matches;
-        mql.addEventListener("change",mqlListener);
-        matches = mql.matches;
-    }
+	function addNewListener(query) {
+		mql = window.matchMedia(query);
+		
 
-    function removeActiveListener() {
-        if (mql && mqlListener) {
-            mql.removeListener(mqlListener);
-        }
-    }
+		mql.addEventListener('change', mqlListener);
+		matches = mql.matches;
+	}
+
+	function removeActiveListener() {
+		if (mql && mqlListener) {
+			mql.removeListener(mqlListener);
+		}
+	}
 </script>
 
 <slot {matches} />
--- src/lib/components/side-bar/SideBar.svelte
+++ src/lib/components/side-bar/SideBar.svelte
@@ -10,21 +10,31 @@
 </script>
 
 <div id="side-bar" class="surface-container" style="width: 100%; height: 100%;">
+	<MediaQuery query="(min-width: 1281px)" let:matches>
+		{#if matches}
+			<div>Large</div>
+		{/if}
+	</MediaQuery>
+
+	<MediaQuery query="(min-width: 601px) and (max-width: 1280px)" let:matches>
+		{#if matches}
+		<div class="bar-container">
+			<div class="nav-container">
+				<HomeIcon />
+				<ScheduleIcon />
+				<TodayIcon />
+				<FileIcon />
+			</div>
+			<div class="theme">
+				<ThemeIcon />
+			</div>
+		</div>
+		{/if}
+	</MediaQuery>
+
 	<MediaQuery query="(max-width: 600px)" let:matches>
 		{#if matches}
-			<div class="">Burger</div>
-		{:else}
-			<div class="bar-container">
-				<div class="nav-container">
-					<HomeIcon />
-					<ScheduleIcon />
-					<TodayIcon />
-					<FileIcon />
-				</div>
-				<div class="theme">
-					<ThemeIcon />
-				</div>
-			</div>
+			<div>Burger</div>
 		{/if}
 	</MediaQuery>
 </div>
