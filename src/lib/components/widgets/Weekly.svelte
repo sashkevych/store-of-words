ed6b5283107fa409011980d91c30ebf6bcbe8fd5
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-06 15:41:29 +0200

    normal white-space

--- src/lib/components/widgets/Weekly.svelte
+++ src/lib/components/widgets/Weekly.svelte
@@ -1,7 +1,7 @@
 <script>
 	import './style.css';
 	import { newData } from '../../../store';
-	
+
 	let weeklyRepeats = [];
 	newData.subscribe((vl) => (weeklyRepeats = vl));
 </script>
@@ -11,13 +11,20 @@
 		<div class="headline-position headline-small on-surface-text">Weekly repeats</div>
 		<div class="repeats-container">
 			{#each weeklyRepeats as { sentences, repeat }}
-				<div class="repeat-box">
-					<div>Repeat: {repeat.count}</div>
+				<div class="repeat-box secondary">
+					<div class="header-position title-medium on-secondary-text">First repeat: {repeat.count}</div>
 					{#each sentences as { id, text }}
-						<div>{id} {@html text}</div>
+						<div class="sentence-position body-small on-secondary-text">
+							<div>{id}. </div>
+							<div class="text-hidden">{@html text}</div>
+						</div>
 					{/each}
 				</div>
 			{/each}
 		</div>
 	</div>
 </div>
+
+<style>
+	
+</style>
