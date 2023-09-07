4d7b02b443f2bea0e0cbd95bac14337bca0135ce
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-07 12:54:09 +0200

    add margin right for boxes

--- src/lib/components/widgets/WorkLog.svelte
+++ src/lib/components/widgets/WorkLog.svelte
@@ -4,12 +4,37 @@
 
 <div id="work-log">
 	<div class="home-widget surface-container">
-		<div class="repeats-template">
-			<div class="headline-position headline-small on-surface-text">Work log</div>
- 			<div class="" />
+		<div class="headline-position headline-small on-surface-text">Work log</div>
+		<div class="repeats-container">
+			
 		</div>
 	</div>
 </div>
 
 <style>
+	.home-widget {
+		height: 100%;
+		border-radius: 30px;
+		padding-top: 1px;
+		margin-top: -1px;
+	}
+
+	.headline-position {
+		margin-top: 30px;
+		margin-left: 55px;
+	}
+	.repeats-container {
+		height: 68%;
+		margin-left: 55px;
+		margin-top: 30px;
+		margin-right: 55px;
+
+		display: flex;
+		white-space: nowrap;
+		flex-wrap: nowrap;
+		overflow: auto;
+
+		gap: 1.5rem;
+	}
+	
 </style>
