9c5d72878c3b6689cfff0d06b82fe237cd80fe6a
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-07 15:38:56 +0200

    add weekly grid

--- src/routes/weekly/+page.svelte
+++ src/routes/weekly/+page.svelte
@@ -2,6 +2,10 @@
 
 </script>
 
-<div>
-    Weekly
+<div class="weekly">
+    <div id="side-bar"></div>
+    <div id="drop-menu"></div>
+    <div id="data"></div>
+    <div id="box-text"></div>
+    <div id="space"></div>
 </div>
