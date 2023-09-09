ad326b82db71321f083083a0c6ea28c4e73354a3
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-09 13:07:21 +0200

    add css for weekly/[id]

--- src/lib/components/widgets/Editor.svelte
+++ src/lib/components/widgets/Editor.svelte
@@ -6,38 +6,30 @@
 		console.log('isOnClickHandler');
 		isFocusDiv.set({ event: true, box_id, sentence_id });
 	}
-
 </script>
 
 <div id="editor">
-	{#key $newData}
-		<div>
-			<div>Day : {box.repeat.count}</div>
-			<div>Box : {box.box_id}</div>
-		</div>
-		<div id={box.box_id} class="border-2 border-green-500 p-2">
+	<div class="editor-box">
+		<div class="headline-large on-surface-text header-text">First repeat</div>
+		<div id={box.box_id} class="distance">
 			{#each box.sentences as { text, id }}
-				<div class="bold">{id}</div>
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
-				<div
-					{id}
-					class="test"
-					contenteditable="true"
-					bind:textContent={text}
-					on:mousedown={() => isOnClickHandler(box.box_id, id)}
-					on:focus={() => onFocusDiv.set(true)}
-					on:blur={() => onFocusDiv.set(false)}
-				/>
+				<div class="sentence body-large on-surface-text">
+					<div>{id}.</div>
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<div
+						{id}
+						class="test"
+						contenteditable="true"
+						bind:textContent={text}
+						on:mousedown={() => isOnClickHandler(box.box_id, id)}
+						on:focus={() => onFocusDiv.set(true)}
+						on:blur={() => onFocusDiv.set(false)}
+					/>
+				</div>
 			{/each}
 		</div>
-	{/key}
+	</div>
 </div>
 
 <style>
-	.test {
-		border: 1px solid seagreen;
-	}
-	.bold {
-		font-weight: bold;
-	}
 </style>
