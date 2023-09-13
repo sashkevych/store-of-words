ab3e2364d97897062a70e7347a613b02ab5a131c
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-13 18:30:07 +0200

    add function set to default

--- src/lib/components/side-bar/ExtraMenu.svelte
+++ src/lib/components/side-bar/ExtraMenu.svelte
@@ -1,24 +1,45 @@
 <script>
-	import { extraMenuData, extraDataIs } from '../../../store';
+	import { isExtraMenu,extraMenuData, extraDataIs } from '../../../store';
 
 	import BackToMainMenu from './BackToMainMenu.svelte';
+
+	function setToDefault(){
+		isExtraMenu.set(false)
+		extraMenuData.set([])
+		extraDataIs.set('')
+	}
 </script>
 
 <div>
 	<div class="nav-position">
 		<BackToMainMenu />
-		{#each $extraMenuData as box}
-			{#if $extraDataIs == 'weekly'}
-				<div>{box.repeat.count}</div>
-			{:else if $extraDataIs == 'also-today'}
-				<div>{box.repeat.data}</div>
-			{/if}
-		{/each}
+		<div class="extra-menu-margin">
+			{#each $extraMenuData as box}
+				{#if $extraDataIs == 'weekly'}
+					<a on:click={setToDefault} href="/weekly/{box.repeat.count}" 
+						><div>First repeat</div></a
+					>
+				{:else if $extraDataIs == 'also-today'}
+					<a on:click={setToDefault} href="/also-today/{box.box_id}" 
+						><div>{box.repeat.data}</div></a
+					>
+					<div>{box.repeat.data}</div>
+				{/if}
+			{/each}
+		</div>
 	</div>
 </div>
 
 <style>
 	.nav-position {
 		margin-top: 42px;
+	}
+
+	.extra-menu-margin {
+		margin-left: 38px;
+		margin-top: 15px;
+	}
+	.link-width {
+		width:fit-content;
 	}
 </style>
