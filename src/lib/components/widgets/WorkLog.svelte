77cef5706dd8cadbf31329c71dfda1890fd08a3f
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-22 10:43:31 +0200

    remove useless div

--- src/lib/components/widgets/WorkLog.svelte
+++ src/lib/components/widgets/WorkLog.svelte
@@ -2,28 +2,23 @@
 	import './style.css';
 
 	import { workLog } from '../../../store';
-
 </script>
 
 <div id="work-log">
 	<div class="home-widget surface-container-low">
 		<div class="worklog-headline-position headline-small on-surface-text">Work log</div>
-		<a href="/work-log">
-			<div class="repeats-container">
-				<div class="secondary-container work-log-sentences-bg">
-					{#each $workLog[0].sentences as { text, id }}
-						<div class="sentence-position body-small on-secondary-container-text header-position">
-							<div>{id}.</div>
-							<div>{text}</div>
-						</div>
-					{/each}
-				</div>
+		<a href="/work-log" class="repeats-container">
+			<div class="secondary-container work-log-sentences-bg">
+				{#each $workLog[0].sentences as { text, id }}
+					<div class="sentence-position body-small on-secondary-container-text header-position">
+						<div>{id}.</div>
+						<div>{text}</div>
+					</div>
+				{/each}
 			</div>
 		</a>
 	</div>
 </div>
 
 <style>
-	
-	
 </style>
