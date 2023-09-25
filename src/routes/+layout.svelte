5f7d49184e50874999a279f7a4307082417dfac9
Aleksandr <naberezhnyy.an@gmail.com>
2023-09-25 17:39:51 +0200

    clear +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,43 +28,6 @@
 			visible.set(false);
 		}
 	}
-	function removeWhiteSpace(newArr) {
-		const res = newArr.filter((box) => {
-			const onlyWithText = box.sentences.filter((sentence, i, arr) => {
-				const IsEmpty = sentence?.text.replace(/&nbsp;/g, '');
-
-				sentence.text = IsEmpty;
-				
-				return {
-					text: IsEmpty,
-					id: sentence.id
-				};
-			});
-			onlyWithText.forEach((sentence, index, arr) => {
-				sentence.id = index + 1;
-			});
-			box.sentences = onlyWithText;
-
-			return box;
-		});
-		return res;
-	}
-	function areThereAnyChanges(newArr, oldArr) {
-		const changes = removeWhiteSpace(newArr).filter((el, i) => {
-			const el1 = el.sentences;
-			const el2 = oldArr[i].sentences;
-			const isAnyChange = el1.some((el, i, arr) => {
-				// if (!el2[i]?.text) return true;
-				// if (!el.text) return true;
-				if (el.text != el2[i].text) return true;
-				if (el2[i + 1]?.text && !arr[i + 1]?.text) return true;
-				return false;
-			});
-
-			if (isAnyChange) return el;
-		});
-		return changes;
-	}
 </script>
 
 <svelte:head>
@@ -75,6 +38,4 @@
 	on:beforeunload={() => close_event_handler($newData, $oldData, $workLog, $oldWorkLog)}
 	on:click={clickHandler}
 />
-<button on:click={() => areThereAnyChanges($newData, $oldData)}>Compare</button>
-<button on:click={() => console.log($newData)}>newData</button>
 <slot />
